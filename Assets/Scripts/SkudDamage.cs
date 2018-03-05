13d7f2f39de1b17f8df128cb23f1478ebd62e166
Unknown <christian.arendt.daniel@gmail.com>
2018-03-05 16:14:11 +0100

    Scripts

---
+++ Assets/Scripts/SkudDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkudDamage : MonoBehaviour {
+    public int Damage = 10;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+       if (other.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<HealthEnemy>().GetRekt(Damage);
+            
+        } 
+    }
+}
