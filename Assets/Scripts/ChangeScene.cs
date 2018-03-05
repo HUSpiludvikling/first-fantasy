20247b197de4f521d9af26ec075b38e7dac864dc
Unknown <christian.arendt.daniel@gmail.com>
2018-03-05 16:24:30 +0100

    More Scripts

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+    public string loadString;
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(loadString);
+    }
+}
---
+++ Assets/Scripts/Points.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour {
+
+    public Text Text;
+
+    private int score = 0;
+
+    private void Awake()
+    {
+        GetScore(0);
+    }
+
+    public void GetScore(int input)
+    {
+        score += input;
+        Text.text = "Points: " + score.ToString();
+    }
+}
