e5f333b3e7f46fa5f980c88b693d7930342affd9
Unknown <christian.arendt.daniel@gmail.com>
2018-04-18 16:56:09 +0200

    battle osv.

---
+++ Assets/BattleDisplayPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleDisplayPlayer : MonoBehaviour {
+
+    public PlayerStats player;
+    public Text HealthText, NameText;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+        NameText.text = "Player";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        HealthText.text = player.health.ToString("0");
+	}
+}
