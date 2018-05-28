d75eb61b00f7633dbfcbd62b6dbb7269a8c3b542
Unknown <christian.arendt.daniel@gmail.com>
2018-05-28 16:40:30 +0200

    Så kan der laves sværd

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -6,7 +6,7 @@ public class Inventory : MonoBehaviour {
     public List<Item> list;
 	// Use this for initialization
 	void Start () {
-        list = new List<Item>();
+		list = new List<Item>();
 	}
 	
 	// Update is called once per frame
---
+++ Assets/ShowItem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ShowItem : MonoBehaviour, IPointerClickHandler {
+    public Item shownitem;
+    Image itemsprite;
+
+    Points points;
+    Inventory inventory;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+        //Hvis man kan købe -
+        if(points.score >= shownitem.Koster)
+        {
+            inventory.list.Add(shownitem);
+            points.score -= shownitem.Koster;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        itemsprite = GetComponent<Image>();
+        points = GameObject.FindGameObjectWithTag("Player").GetComponent<Points>();
+
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        itemsprite.sprite = shownitem.sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+
+}
